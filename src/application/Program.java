70774fec2428bcebe7315987eac5973a122950e9
KellyCarvalho <18kellycarvalho@gmail.com>
2020-12-07 19:50:07 -0300

    Adicionando classes - inicio

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import domain.entities.Departamento;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+Departamento obj = new Departamento(1,"Humano");
 
+System.out.println(obj);
 	}
 
 }
